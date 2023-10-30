754f72a9c95463efcf04c49bc823223e400a875e
Neidiana Rodrigues Martins <94389802+Neidiana@users.noreply.github.com>
2023-10-30 15:21:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,70 @@
-var speechRecognition=window.webKitspeechRecognition;
-var recoginition=speechRecognition();
-var textBox=document.getElementsById("textBox");
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
 
-function start(){
-    textBox.innerHTML=" ";
-    recoginition .start();
-}
- recoginition.onResult=function(event){
-    console.log(event);
-    var Content=event.results[0] [0].transcript;
-    textBox.innerHTML= Content;
+var Textbox = document.getElementById("textbox"); 
+
+function start()
+{
+    Textbox.innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    Textbox.innerHTML = Content;
     console.log(Content);
-    if(Content=="tire minha selfie"){
-        console.log("tirando selfie---");
+      if(Content =="tire minha selfie")
+      {
+        console.log("tirando selfie --- ");
         speak();
-    }
- }
+      }
+}
 
- function speak(){
-var synth=window.speechSynthesis;
-speakData="tirando sua selfie em 5 segundos";
-var utterThis=new SpeechSynthesisUtterance(speakData);
-synth.speak(utterThis);
-Webcam.attach(camera);
-setTimeout(function() {
-    takeSelfie();
-    Save(); 
-}, 5000 );
- }
 
- camera=document.getElementById("Camera");
- Webcam.Set({
-    whidth:360,
-    Height:250,
-    Image_format:'JPeg',
-    Jpeg_Qualit:90
- });
+function speak(){
+    var synth = window.speechSynthesis;
 
- function takeSelfie() {
-Webcam.snep(function(Data_uri){
-    Document.getElementById("result").innerHTML='<img id="selfieImage"src="'+data_uri+'"/>';
+    speakData = "Tirando sua selfie em 5 segundos";
+
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        takeSelfie(); 
+        save();
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
 });
- }
 
- function Save(){
-    Link=document.getElementById("link");
-    Image=document.getElementById("selfieImage").src;
-    link.href=Image;
-    link.clik();
- }
+function takeSelfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfieImage" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfieImage").src ;
+  link.href = image;
+  link.click();
+}
